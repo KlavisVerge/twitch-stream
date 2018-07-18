99df76e52c98ebf1b32251633cbfbc74dfe50a23
Nick Phillips <imprecatorr@gmail.com>
2018-07-17 22:33:36 -0500

    initial commit

---
+++ src/twitch-stream-app/twitch-stream-app.js
@@ -0,0 +1,43 @@
+import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+
+/**
+ * @customElement
+ * @polymer
+ */
+class TwitchStreamApp extends PolymerElement {
+  static get template() {
+    return html`
+      <style>
+        :host {
+          display: flex;
+          flex-direction: column;
+          flex-wrap: wrap;
+          justify-content: center;
+          text-align: center;
+        }
+      </style>
+      <iron-image src="[[imgsrc]]"></iron-image>
+      <paper-item>Title: [[title]]</paper-item>
+      <paper-item>Viewer Count: [[viewerCount]]</paper-item>
+      <paper-item>Started at: [[startedAt]]</paper-item>
+    `;
+  }
+  static get properties() {
+    return {
+      imgsrc: {
+        type: String
+      },
+      title: {
+        type: String
+      },
+      viewerCount: {
+        type: String
+      },
+      startedAt: {
+        type: String
+      }
+    };
+  }
+}
+
+window.customElements.define('twitch-stream-app', TwitchStreamApp);
